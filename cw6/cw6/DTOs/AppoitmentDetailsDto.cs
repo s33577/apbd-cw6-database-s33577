6f08a4b3fa892fec46ae212858a996314ae27dda
Andrew Kotowiecki <s33577@pjwstk.edu.pl>
2026-04-24 21:41:50 +0200

    Dtos created.

---
+++ cw6/cw6/DTOs/AppoitmentDetailsDto.cs
@@ -0,0 +1,10 @@
+namespace Tutorial7.DTOs;
+
+public class AppoitmentDetailsDto : AppointmentListDto
+{
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string DoctorFullName { get; set; } = string.Empty;
+    public string LicenseNumber { get; set; } = string.Empty;
+    public string? InternalNotes { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
